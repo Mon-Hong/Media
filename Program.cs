d2634a869c36baeda710f7663d006cc2b6ec94ad
Borwonlak Phothong <78490883+Mon-Hong@users.noreply.github.com>
2021-04-24 15:49:17 +0700

    บันไดสวรรค์

--- Program.cs
+++ Program.cs
@@ -1,96 +1,45 @@
 ﻿using System;
 
-namespace Computer_Programming_for_Media
+namespace Heaven_stairs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int day, time;
+            int width, high, step, x, num = 0, width1 = 0;
+            char letter;
+            Console.Write(" input letter :");
+            letter = char.Parse(Console.ReadLine());
 
-            Console.Write("Input day : ");
-            day = int.Parse(Console.ReadLine());
+            Console.Write(" input width :");
+            width = int.Parse(Console.ReadLine());
+            Console.Write(" input high :");
+            high = int.Parse(Console.ReadLine());
+            Console.Write(" input step :");
+            step = int.Parse(Console.ReadLine());
 
-            Console.Write("Input time : ");
-            time = int.Parse(Console.ReadLine());
-
-
-            int breakfast, weekend, coffe;
-
-            breakfast = 5;
-            weekend = 5;
-            coffe = 2;
-
-            string menu;
-
+            width1 = width;
             do
             {
-                Console.Write("Input menu : ");
-                menu = Console.ReadLine();
-
-                if (menu == "Breakfast")
+                num++;
+                for (int i = 1; i <= high; i++)
                 {
-                    if (time > 11)
+                    for (int j = 1; j <= width; j++)
                     {
-                        Console.WriteLine("Sorry your order is not available");
-                    }
-                    if (breakfast == 0)
-                    {
-                        Console.WriteLine("Sorry your order is out of stock");
-                    }
-                    if (time >= 8 && time <= 11 && breakfast != 0)
-                    {
-                        breakfast = breakfast - 1;
+
+                        Console.Write(letter);
+
                     }
 
+                    Console.WriteLine();
                 }
 
-                if (menu == "Weekend")
-                {
-                    if (day >= 1 && day <= 5)
-                    {
-                        Console.WriteLine("Sorry your order is not available");
-                    }
-                    if (weekend == 0)
-                    {
-                        Console.WriteLine("Sorry your order is out of stock");
-                    }
-                    if (day == 6 || day == 7 && time >= 8 && time <= 18 && weekend != 0)
-                    {
-                        weekend = weekend - 1;
-                    }
+                width = width1 + width;
 
-                }
-
-                if (menu == "Coffe")
-                {
-                    if (coffe == 0)
-                    {
-                        Console.WriteLine("Sorry your order is out of stock");
-                    }
-                    if (day >= 1 && day <= 7 && time >= 8 && time <= 18 && coffe != 0)
-                    {
-                        coffe = coffe - 1;
-                    }
-                }
-                if (menu == "End")
-                {
-
-                }
-
-                else
-                {
-                    Console.WriteLine("Please enter a valid menu");
-
-                }
-
-            }
-
-            while (menu != "End");
-
-
+            } while (num < step);
 
             Console.ReadLine();
+
         }
     }
 }
